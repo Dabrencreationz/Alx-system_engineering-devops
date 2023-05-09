efdbd7366c908dac518e7feca451d80000b23317
Brenda <brenvaltessy@yahoo.com>
2023-05-09 14:29:17 -0700

    advance api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# function that queries the Reddit API and returns the number of subscribers
+import requests
+
+
+def number_of_subscribers(subreddit);
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    #send GET to the API
+    response = requests.get(url, headers=headers, allow_redirections=False)
+
+    # If request was successful, return the number of subs
+    if response.statud_code == 200:
+        return response.json()['data']['subscribers']
+
+    # If reddit is invalid, return 0
+elif return.status_code == 404:
+    return 0
+
+else:
+    response.raise_for_status()
